22399f513d67e4fb5bfe8cc5a3c8d027a4876e5f
Ulysses Sousa <ulyssessousa@gmail.com>
2024-03-18 16:22:24 -0300

    Criação da superclasse Animal

---
+++ src/exemploanimal/Animal.java
@@ -0,0 +1,32 @@
+package exemploanimal;
+
+public abstract class Animal {
+
+	private String nome;
+	private String tipo;
+	
+	public Animal(String nome, String tipo) {
+		super();
+		this.nome = nome;
+		this.tipo = tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public abstract String emitirSom();
+	
+}
---
+++ src/principal/Principal.java
@@ -0,0 +1,12 @@
+package principal;
+
+import exemploanimal.Animal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
